87dc44a154f8237aff80985407c3534d946b23e2
Ivan Berezutskiy <santiago_bravo@mail.ru>
2024-09-29 14:10:17 +0300

    написали на занятии

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -4,14 +4,12 @@ import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 public class Main {
     public static void main(String[] args) {
-
-
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
-            botsApplication.registerBot("token", new MessageListenerHandler());
+
+            botsApplication.registerBot("7727206414:AAHKbGMYVtveLD0XODj2xIJ8q6N213lueQ8", new MessageListenerHandler());
 
 
             Thread.currentThread().join();
-
         } catch (Exception e) {
             System.err.println("Error while registering bot  " + e.getMessage());
         }
--- src/main/java/ru/itpark/MessageListenerHandler.java
+++ src/main/java/ru/itpark/MessageListenerHandler.java
@@ -5,17 +5,67 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 import java.util.Random;
 
 public class MessageListenerHandler implements LongPollingSingleThreadUpdateConsumer {
-    TelegramClient client = new OkHttpTelegramClient("token");
+    TelegramClient client = new OkHttpTelegramClient("7727206414:AAHKbGMYVtveLD0XODj2xIJ8q6N213lueQ8");
     Random randomGenerator = new Random();
+    int myNumber = randomGenerator.nextInt(0, 100);
 
-    @SneakyThrows
     @Override
     public void consume(Update update) {
-       
+        if (update.hasMessage() == true) {
+            Message messageFromUser = update.getMessage();
+
+            if (messageFromUser.hasText() == true) {
+                String textFromMessage = messageFromUser.getText();
+
+                if (textFromMessage.equals("/start") == true) {
+                    SendMessage messageToSend = new SendMessage(messageFromUser.getChatId().toString(),
+                            "Привет! Я загадал число от 0 до 100! Попробуй угадать");
+
+                    try {
+                        client.execute(messageToSend);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    int guessedNumber = Integer.parseInt(textFromMessage);
+
+                    if (guessedNumber == myNumber) {
+                        SendMessage messageToSend = new SendMessage(messageFromUser.getChatId().toString(),
+                                "Ты угадал число");
+
+                        try {
+                            client.execute(messageToSend);
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else if (myNumber > guessedNumber) {
+                        SendMessage messageToSend = new SendMessage(messageFromUser.getChatId().toString(),
+                                "Мое число больше твоего");
+
+                        try {
+                            client.execute(messageToSend);
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else if (myNumber < guessedNumber) {
+                        SendMessage messageToSend = new SendMessage(messageFromUser.getChatId().toString(),
+                                "Мое число меньше твоего");
+
+                        try {
+                            client.execute(messageToSend);
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
